fabdd1a3fbc4a7654c3f23efc0171a2a038cd87f
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-06-06 15:24:42 -0700

    8-uppercase.py adding

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,13 @@
+#!usr/bin/python3
+def uppercase(str):
+    result = ""
+    for i in range(len(str)):
+        if str[i] != " ":
+            x = ord(str[i])
+            if x > 90:
+                y = x - 32
+                result = result + "{:c}".format(y)
+            else:
+                result = result + ":c".format(x)
+    print(result)
+
