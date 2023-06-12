180b78c0f10494820fe23e99646e2ec06a336882
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-06-12 16:45:42 -0700

    6-print_matrix_integer.py

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    i = 0
+    while i < len(matrix):
+        for j in range(len(matrix[i])):
+            print("{:d}".format(matrix[i][j]), end=" ")
+        print()
+        i += 1 
+
