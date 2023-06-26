f48d8fd25fbe6af2ad892f25532aa541728f1a02
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-06-26 05:30:36 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,10 +5,9 @@ def safe_print_list(my_list=[], x=0):
     for i in range(x):
         try:
             print(my_list[i], end="")
-        except IndexError:
-            print("\n")
+        except:
             return total_len
         else:
             total_len += 1
-
+    print("\n")    
     return total_len
