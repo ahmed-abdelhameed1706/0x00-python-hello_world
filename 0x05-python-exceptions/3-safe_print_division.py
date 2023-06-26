0155e90406506d227181603477e9c0518567f938
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-06-26 06:31:39 -0700

    3-safe_print_division.py

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_division(a, b):
+    try:
+        value = a / b
+    except ZeroDivisionError:
+        value = "None"
+    finally:
+        print("Inside Result: {}".format(value))
+
+    return value
