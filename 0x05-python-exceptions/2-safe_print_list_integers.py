0ffc757bd9ce0bfee4635d2b4a15e3d2ecc9280f
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-06-26 06:11:28 -0700

    2-safe_print_list_integers.py

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -3,12 +3,11 @@
 def safe_print_list_integers(my_list=[], x=0):
 
     total_len = 0
-    for i in my_list:
+    for i in range(x):
         try:
-            print("{:d}".format(i), end="")
+            print("{:d}".format(my_list[i]), end="")
         except IndexError:
-            print()
-            print("List Index is bigger than list len")
+            return print("List Index is bigger than list len")
         except (ValueError, TypeError):
             continue
         else:
