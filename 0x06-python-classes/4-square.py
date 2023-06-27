c4105d6d754f7851251c5df361de0f1b9c18e217
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-06-27 14:35:57 -0700

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""defines a class square and validate it's size"""
+
+
+class Square:
+    """
+    this is the class square
+    """
+
+    def __init__(self, size=0):
+        """size is an integer"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.getter
+    def size(self, value):
+        self.__size = value
