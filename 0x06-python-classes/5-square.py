7f7c9e52949e50c6be5dfbe1d8dd5c309afbcfa4
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-06-27 14:46:46 -0700

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -31,7 +31,10 @@ class Square:
         return self.__size ** 2
 
     def my_print(self):
-        for i in range(self.__size):
-            for j in range(self.__size):
-                print("#", end="")
+        if self.__size == 0:
             print()
+        elif:    
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
