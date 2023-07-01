e6b28c5842a805b0af9060736795bafb437063ea
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-07-01 10:49:08 -0700

    task 1

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """
+    a function that adds 2 integers
+    a: an integer
+    b: an integer
+    Returns a + b
+    """
+
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
