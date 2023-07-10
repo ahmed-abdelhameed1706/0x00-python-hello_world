6a469906bf90ff1c0db8b68ec0af790df9c7da5f
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-07-10 02:24:59 -0700

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,12 +8,8 @@ class MyList(list):
     """
     a class inhirits from the list
     """
-    def __init__(self):
-        super().__init__(self)
-
     def print_sorted(self):
         """
         prints the list sorted
-
         """
         print(sorted(self))
