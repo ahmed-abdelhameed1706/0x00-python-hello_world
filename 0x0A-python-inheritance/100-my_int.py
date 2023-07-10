07ea166f7694eb86878ca7207720c6163478cd9d
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-07-10 02:43:57 -0700

    100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -5,13 +5,22 @@ function inhirits from int and reverts operators
 
 
 class MyInt(int):
+    """
+    a class to invert eq with ne
+    """
     def __eq__(self, other):
+        """
+        an equal function to invert
+        """
         if int(self) == int(other):
             return False
         else:
             return True
 
     def __ne__(self, other):
+        """
+        a not equal function to invert with eq
+        """
         if int(self) != int(other):
             return False
         else:
