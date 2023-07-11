3cec562cacb0577ec1eb208591cb214c074f9fef
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-07-11 08:34:57 -0700

    3-to_json_string.py

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+function to return a json string from obj
+"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """
+    function to return a json string from obj
+    """
+    return json.dumps(my_obj)
