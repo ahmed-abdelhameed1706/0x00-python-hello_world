88e71ed45559326c48ad7e9bb5a763f37d146abc
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-07-11 07:03:43 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,10 +4,13 @@ a function that reads a text from a file
 """
 
 
+import sys
+
+
 def read_file(filename=""):
     """
     function that reads a text from a file
     filename is the filename
     """
     with open(filename, 'r', encoding="UTF-8") as f:
-        print(f.read())
+        print(f.read(), file=sys.stdout)
