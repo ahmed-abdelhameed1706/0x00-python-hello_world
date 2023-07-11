f8270fae68a0eb6c948053d3dd22686f867a7eb1
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-07-11 09:29:42 -0700

    10-student.py

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+a student class
+"""
+
+
+class Student:
+    """
+    a class that reprents a student
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is not None:
+            jdict = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    jdict[key] = value
+            return jdict
+        return self.__dict__
