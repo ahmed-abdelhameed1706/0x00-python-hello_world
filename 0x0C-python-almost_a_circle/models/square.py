2704f5ccde9708a46db4b70a0304d4519de102c6
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-07-17 08:40:33 -0700

    models/square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -43,6 +43,9 @@ class Square(Rectangle):
                 setattr(self, key, value)
 
     def to_dictionary(self):
-        self_dict = {"id": self.id, "size": self.size,
-                    "x": self.x, "y": self.y}
+        self_dict = {
+                "id": self.id,
+                "size": self.size,
+                "x": self.x,
+                "y": self.y}
         return self_dict
