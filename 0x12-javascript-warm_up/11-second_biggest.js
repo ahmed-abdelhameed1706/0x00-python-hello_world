b32ebdb6343c6e0345de88b7bc1cc938011f9e79
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-09-11 02:58:49 -0700

    11-second_biggest.js

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const length = process.argv.length;
+const list = process.argv;
+
+if (length <= 3) {
+  console.log('0');
+} else {
+  list.sort();
+  list.reverse();
+  console.log(list[1]);
+}
