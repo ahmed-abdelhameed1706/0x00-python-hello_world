f349685581e61b7a1ae8e4ffe21385755faf2451
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-09-11 03:32:39 -0700

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
+// Searching for second biggest number
 
 const length = process.argv.length;
 
 if (length <= 3) {
-  console.log('0');
+  console.log(0);
 } else {
   const list = process.argv;
   list.sort().reverse();
-  console.log(parseInt(list[1]));
+  console.log(list[1]);
 }
