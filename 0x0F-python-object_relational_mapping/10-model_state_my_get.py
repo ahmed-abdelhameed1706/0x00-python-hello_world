f40ab1fbefaeea9ad03fc6c37a4ad4a1f6c5dbf5
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-09-15 23:17:30 -0700

    10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+script that prints the State object with the name passed
+as argument from the database hbtn_0e_6_usa
+"""
 from model_state import Base, State
 import sys
 from sqlalchemy import create_engine
@@ -11,14 +15,15 @@ if __name__ == "__main__":
     arg3 = sys.argv[3]
     arg4 = sys.argv[4]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(arg1, arg2, arg3), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        arg1, arg2, arg3), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
 
     session = Session()
-    
-    state = session.query(State).filter(State.name==arg4).first()
+
+    state = session.query(State).filter(State.name == arg4).first()
 
     if state:
         print("{}".format(state.id))
