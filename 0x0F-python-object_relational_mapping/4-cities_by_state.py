c33a093efe97dcd1af1880137ecc2fe2f0d2f68f
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-09-15 23:06:02 -0700

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
 '''
 script to get states
 '''
+import MySQLdb
+import sys
 
 if __name__ == '__main__':
 
@@ -11,18 +11,17 @@ if __name__ == '__main__':
     arg2 = sys.argv[2]
     arg3 = sys.argv[3]
 
-    conn = MySQLdb.connect(host="localhost", port=3306, user=arg1, passwd=arg2, db=arg3, charset="utf8")
+    conn = MySQLdb.connect(host="localhost", port=3306, user=arg1,
+                                passwd=arg2, db=arg3, charset="utf8")
 
     cur = conn.cursor()
-    
-    
+
     query = '''
     SELECT cities.id, cities.name, states.name
     FROM cities
     JOIN states ON cities.state_id = states.id
     ORDER BY cities.id ASC;
     '''
-    
 
     cur.execute(query)
 
