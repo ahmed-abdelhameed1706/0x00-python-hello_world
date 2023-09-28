e3b197a43b1e3f1702979fd9d7ced5faaa23fb77
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-09-28 07:30:00 -0700

    6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+function to find the peak in a list of numbers
+"""
+
 
 def find_peak(list_of_integers):
     """finds a peak in a list of numbers"""
@@ -10,7 +14,7 @@ def find_peak(list_of_integers):
         return max(list_of_integers)
     mid = int(len(list_of_integers) / 2)
     if list_of_integers[mid] >= list_of_integers[mid - 1] and\
-    list_of_integers[mid] >= list_of_integers[mid + 1]:
+            list_of_integers[mid] >= list_of_integers[mid + 1]:
         return list_of_integers[mid]
     if list_of_integers[mid] < list_of_integers[mid - 1]:
         return find_peak(list_of_integers[:mid])
