962565a9b1fb79bac57a3f3ca526dbc4a8de005a
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-09-28 07:49:20 -0700

    101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # getting the content of a respone with curl
-curl -X POST -H "Content-Type: application/json" -d "$(cat $2)" $1
+curl -X POST $1 -H "Content-Type: application/json" -d "$(cat $2)"
