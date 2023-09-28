d5d316b1dd43a4233acfb074a094499831deb5a9
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-09-28 07:26:46 -0700

    6-peak.py

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def find_peak(list_of_integers):
+    """finds a peak in a list of numbers"""
+    if not list_of_integers:
+        return None
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
+    if len(list_of_integers) == 2:
+        return max(list_of_integers)
+    mid = int(len(list_of_integers) / 2)
+    if list_of_integers[mid] >= list_of_integers[mid - 1] and\
+    list_of_integers[mid] >= list_of_integers[mid + 1]:
+        return list_of_integers[mid]
+    if list_of_integers[mid] < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    if list_of_integers[mid] < list_of_integers[mid + 1]:
+        return find_peak(list_of_integers[mid + 1:])
