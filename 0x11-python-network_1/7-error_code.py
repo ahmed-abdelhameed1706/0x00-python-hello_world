655226af7f31c5ac03562f2a38272a742edbd676
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-09-29 08:01:56 -0700

    7-error_code.py

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+script to send request and print response and catch errors
+"""
+
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+
+    if r.status_code >= 400:
+        print(f"Error code: {r.status_code}")
+    else:
+        print(r.text)
