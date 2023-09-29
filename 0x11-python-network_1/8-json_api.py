dcfa887e8a1ec9282f7d18d4e95f27bb33c7461f
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-09-29 08:24:39 -0700

    8-json_api.py

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+script to search user
+"""
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    letter = sys.argv[1] if len(sys.argv) > 1 else ""
+    
+    url = "http://0.0.0.0:5000/search_user"
+
+    q = ''
+
+    if letter:
+        q = letter
+
+    data = {"q": q}
+
+    r = requests.post(url, data=data)
+    
+    try:
+        if r.json():
+            if 'id' in r.json() and "name" in r.json():
+                print(f"[{r.json()['id']}] {r.json()['name']}")
+            else:
+                print("No result")
+        else:
+            print("No result")
+    except:
+        print("Not a valid JSON")
