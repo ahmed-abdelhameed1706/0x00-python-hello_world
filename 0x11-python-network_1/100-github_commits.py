08a7ca43275f7925433eb10623aacee985644868
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-09-29 09:09:17 -0700

    100-github_commits.py

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,22 +11,14 @@ import sys
 if __name__ == "__main__":
     repo_name = sys.argv[1]
     owner = sys.argv[2]
-    
+
     url = f"https://api.github.com/repos/{owner}/{repo_name}/commits"
 
-    
-    params = {
-            "per_page": 10,
-            "sort": "committer-date",
-            }
-
-
-    r = requests.get(url, params=params)
+    r = requests.get(url)
 
     commits = r.json()
-    commits.reverse()
-    for commit in commits:
-        sha = commit['sha']
-        author_name = commit['commit']['author']['name']
+
+    for i in range(10):
+        sha = commits[i].get("sha")
+        author_name = commits[i].get('commit').get('author').get('name')
         print(f"{sha} {author_name}")
-        #print(f"{commit['sha']} {commit['commit']['author']['name']}")
