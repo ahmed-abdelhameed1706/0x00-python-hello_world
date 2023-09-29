5af535f9697c72cdc4e32034da20982e27b93043
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-09-29 07:40:07 -0700

    4-hbtn_status.py

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+script to fetch a url using requests
+"""
+
+
+import requests
+
+
+if __name__ == "__main__":
+
+    response = requests.get('https://alx-intranet.hbtn.io/status').text
+
+    print("Body response:")
+    print(f"\t- type: {type(response)}")
+    print(f"\t- content: {response}")
