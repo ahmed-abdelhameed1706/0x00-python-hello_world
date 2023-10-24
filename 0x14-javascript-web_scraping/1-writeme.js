6cdc9b90713c8ed2537875eec26aa370595e22a4
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-10-24 02:03:49 -0700

    adding first 2 tasks

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const fs = require("fs")
+
+const filepath = process.argv[2]
+
+const stringToWrite = process.argv[3]
+
+fs.writeFile(filepath, stringToWrite, 'utf8', (err) => {
+	if (err) {
+		console.error(err);
+	}
+});
