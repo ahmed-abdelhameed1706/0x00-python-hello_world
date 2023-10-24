b34b868137f688075a76d610faf898075c45b260
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-10-24 02:06:00 -0700

    fixing semistandard

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
-const fs = require("fs")
+const fs = require('fs');
 
-const filepath = process.argv[2]
+const filepath = process.argv[2];
 
-const stringToWrite = process.argv[3]
+const stringToWrite = process.argv[3];
 
 fs.writeFile(filepath, stringToWrite, 'utf8', (err) => {
-	if (err) {
-		console.error(err);
-	}
+  if (err) {
+    console.error(err);
+  }
 });
